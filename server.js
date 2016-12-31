e46b4e741af17820a35f8ef08431bc7c04ae40f5
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-12-31 10:56:54 -0800

    added mongoose and bodyparser

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
 app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+
+});
+
 app.listen(process.env.PORT || 8080);
 
 exports.app = app;
